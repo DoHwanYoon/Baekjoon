337d18dea37a19eb63bc6e547d33c6f18e051760
DoHwanYoon <heesosik4you@naver.com>
2019-05-07 11:54:14 +0900

    2019.05.07

---
+++ src/Operation/B1000.java
@@ -0,0 +1,17 @@
+package Operation;
+
+import java.util.Scanner;
+
+public class B1000 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		int a, b;
+		a = scan.nextInt();
+		b = scan.nextInt();
+		if (a > 0 && b < 10) {
+			System.out.println(a + b);
+		} else {
+			System.out.println("a>0, b<10조건을 만족하세요.");
+		}
+	}
+}
