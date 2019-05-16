50cbcfd960b9d945941ac1070dc638f5a978a4b1
DoHwanYoon <heesosik4you@naver.com>
2019-05-16 19:14:52 +0900

    if문 연습

---
+++ src/If/B10817.java
@@ -0,0 +1,13 @@
+package If;
+
+import java.util.Scanner;
+public class B10817 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] num = new int[3];
+		
+
+	}
+
+}
