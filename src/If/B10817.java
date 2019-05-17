e3a4a5993ab42fc959126593b1b4bb75bfc1ce51
DoHwanYoon <heesosik4you@naver.com>
2019-05-17 09:33:16 +0900

    if문 연습

--- src/If/B10817.java
+++ src/If/B10817.java
@@ -5,8 +5,20 @@ public class B10817 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int[] num = new int[3];
-		
+        int x,y,z;
+        x=scan.nextInt();
+        y=scan.nextInt();
+        z=scan.nextInt();
+
+        if((y<=x && x<=z)||(z<=x && x<=y)){
+            System.out.print(x);
+        }
+        else if((x<=y && y<=z)||(z<=y && y<=x)){
+            System.out.print(y);
+        }
+        else{
+            System.out.print(z);
+        }
 
 	}
 
