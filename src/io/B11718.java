805dcd279bc1949e1b0f63b033d97f0f0623c91e
DoHwanYoon <heesosik4you@naver.com>
2019-05-27 16:29:25 +0900

    백준 11718

---
+++ src/io/B11718.java
@@ -0,0 +1,14 @@
+package io;
+
+import java.util.Scanner;
+public class B11718 {
+	    public static void main(String[] args){
+	       Scanner scan = new Scanner(System.in);
+	            String in = "";
+	            while(scan.hasNextLine()){
+	                in = scan.nextLine();
+	                System.out.println(in);
+	            }
+	            scan.close();
+	    }
+	}
